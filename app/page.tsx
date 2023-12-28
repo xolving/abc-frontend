9c568d580ce4da98c673d5242d69e1d5d00bf573
enbraining <me@xolving.com>
2023-12-28 13:48:26 +0900

    update layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
-            <Link key="/posts" href="/posts" className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
+            <Link key="/posts" href="/posts" className="text-sm duration-200 text-zinc-500 hover:text-zinc-300">
               Posts
             </Link>
-            <Link key="/contact" href="/contact" className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
+            <Link key="/contact" href="/contact" className="text-sm duration-200 text-zinc-500 hover:text-zinc-300">
               Contact
             </Link>
         </ul>
@@ -19,7 +19,7 @@ export default function Home() {
         className="absolute inset-0 -z-50 animate-fade-in"
         quantity={70}
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text pb-4">
+      <h1 className="z-10 text-4xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text pb-4">
         enbraining
       </h1>
 
@@ -27,7 +27,7 @@ export default function Home() {
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
           I'm building{" "}
-          <Link href="https://xolving.com" className="underline duration-500 hover:text-zinc-300">
+          <Link href="https://xolving.com" className="underline duration-299 hover:text-zinc-300">
             xolving.com
           </Link>
         </h2>
