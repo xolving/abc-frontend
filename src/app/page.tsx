a14ae05cbcce938fc5660d285a3420734699abe4
enbraining <me@xolving.com>
2023-12-29 16:17:16 +0900

    hmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import { allRoadmaps } from "@/.contentlayer/generated";
 import Link from "next/link";
+import { Card } from "../components/card";
+import { Article } from "./roadmap/article";
 
 export default function Home() {
+  const sorted = allRoadmaps
+    .filter((p) => p.published)
+    .sort(
+      (a, b) =>
+        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+    );
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-10 animate-fade-in">
@@ -20,6 +31,19 @@ export default function Home() {
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in" />
+
+      <div className="z-10 text-4xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text pb-4 font-bold">
+      <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-1">
+          <div className="grid grid-cols-3 gap-4">
+            {sorted
+              .map((roadmap) => (
+                <Card key={roadmap.slug}>
+                  <Article roadmap={roadmap} views={0} />
+                </Card>
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 
