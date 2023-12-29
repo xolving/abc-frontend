ea3d15c06c98ab0ba4d947b86526b08d2c05cbd4
enbraining <me@xolving.com>
2023-12-29 18:35:17 +0900

    먼지 쪼가리 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { allRoadmaps } from "@/.contentlayer/generated";
 import Link from "next/link";
 import { Card } from "../components/card";
+import Particles from "../components/particles";
 import { Article } from "./roadmap/article";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <Particles className="absolute inset-0 -z-50 animate-fade-in"/>
       <div className="my-10 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
             <Link key="/roadmap" href="/roadmap" className="text-sm duration-200 text-zinc-400 hover:text-zinc-200">
@@ -23,7 +25,7 @@ export default function Home() {
               About
             </Link>
         </ul>
-      </div>
+      P</div>
       
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <h1 className="z-10 text-4xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-8xl whitespace-nowrap bg-clip-text pb-2 font-bold">
