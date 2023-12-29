78035e775b440ed96eec894722e2f8c98b1f278b
enbraining <me@xolving.com>
2023-12-29 18:28:07 +0900

    optimizing animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       </div>
       
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <h1 className="z-10 text-4xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text pb-4 font-bold">
+      <h1 className="z-10 text-4xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-8xl whitespace-nowrap bg-clip-text pb-2 font-bold">
         roadmap
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
